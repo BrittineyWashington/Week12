08c8d0352828932bf86f97ab4ce825fd5fa550c1
BrittineyWashington <https://github.com>
2023-09-03 02:30:12 -0400

    2nd push create template files for project

---
+++ song.js
@@ -0,0 +1,9 @@
+//This is my Week 12 Final Unit Project JS File
+//Song Manager
+// console.log("Hello World... Song Manager is my Week 12 Project!");
+
+//this function takes a songGroup object and POSTs that into the API using ajax
+function createSongGroup(songGroup) {
+    console.log("createSongGroup songGroup: ", songGroup);//this logs the songGroup object to be POSTED
+}
+
