d1eed3d6b7d03c08acc9d170ae568b44b37b4d97
BrittineyWashington <https://github.com>
2023-09-03 02:49:48 -0400

    3rd push updated html added form and table code, and updt js

--- song.js
+++ song.js
@@ -1,3 +1,11 @@
+//Coding Steps:
+// Create a full CRUD application of your choice using an API or JSON Server.
+// Use JQuery/AJAX to interact with the API. 
+// Use a form to post new entities.
+// Build a way for users to update or delete entities.
+// Include a way to get entities from the API.
+// Use Bootstrap and CSS to style your project.
+
 //This is my Week 12 Final Unit Project JS File
 //Song Manager
 // console.log("Hello World... Song Manager is my Week 12 Project!");
